beb25cdd9da8c0173e327f4393c223e4106c0888
Anderson Mello <andersonmello722@gmail.com>
2022-06-03 16:32:05 -0300

    Requisito 3

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -3,6 +3,27 @@ const { fetchItem } = require('../helpers/fetchItem');
 const item = require('../mocks/item');
 
 describe('2 - Teste a função fetchItem', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Testa se é uma função', () => {
+    expect(typeof fetchItem).toBe('function');
+  });
+  it('testa se fetch é chamado com a função fetchItem', async () => {
+    expect.assertions(1)
+    await fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalled();
+  });
+  it('Testa se a função fetch é chama com oend point "https://api.mercadolibre.com/items/MLB1615760527"', async () => {
+    const url = "https://api.mercadolibre.com/items/MLB1615760527";
+    await fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalledWith(url);
+  });
+  it('Testa se o retorno da função é igual ao objeto \'item\'', async () => {
+    expect(await fetchItem('MLB1615760527')).toEqual(item);
+  });
+  it('Testa se a função retorna o erro \'You must provide an url\'', async () => {
+    try {
+      await fetchItem();
+    } catch (error) {
+      expect(error).toEqual(new Error('You must provide an url'));
+    }
+  });
 });
